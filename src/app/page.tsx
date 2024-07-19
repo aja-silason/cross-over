1a15b61f19a32273be00855306d35e93c5333d22
Anania Augusto <anania.augusto@baxtter.local>
2024-07-19 13:15:32 +0100

    wip: send project in the repository

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <span className="flex text-[20pt]">
+      Pagina do website da cross-over
+    </span>
+    
+  );
+}
